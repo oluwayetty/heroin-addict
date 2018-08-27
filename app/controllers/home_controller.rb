8b75f9fdf122782d53251612da3004f58547ed9a
Yetunde Sanni <yetunde.sanni@andela.com>
2018-08-27 14:08:07 +0100

    low status feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @last_moods = current_user.daily_moods.last(2).pluck(:mood).uniq
-    @low_status = @last_moods.include? "Depressed" || "Bad"
+    @low_status = @last_moods.any? { |s| ['Depressed', 'Bad'].any? { |t| s.include?(t) } }
   end
 end
