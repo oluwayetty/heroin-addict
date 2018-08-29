000c713191882f12b6b8e685778fdbe14a68ec68
Akeem Oduola <akeem.oduola@andela.com>
2018-08-29 17:43:43 +0100

    Forgot Password feature added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -53,4 +53,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Setup the mailer config
+  config.action_mailer.perform_deliveries = true
 end
