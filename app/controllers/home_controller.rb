30dc718a01a074e3ef52c341d004ff32209f4fb9
Yetunde Sanni <yetunde.sanni@andela.com>
2018-08-29 19:03:28 +0100

    small design changes for sticky footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:about]
+
   def index
     @last_moods = current_user.daily_moods.last(2).pluck(:mood).uniq
     @low_status = @last_moods.any? { |s| ['Depressed', 'Bad'].any? { |t| s.include?(t) } }
